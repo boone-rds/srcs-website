5dc7180636446800a2fd8fc199123b814e3d8f64
boone-rds <boone@raneydaysolutions.com>
2026-08-21 16:57:48 -0500

    Initialize SRCS website

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <main>
+      <section>
+        <h1>Soil-Right Consulting Services</h1>
+        <p>SRCS website rebuild is underway.</p>
+      </section>
+    </main>
+  );
+}
+
+export default App;
